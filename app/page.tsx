80c0c4ffb1c718f7731da26b2340fdcd06aa81de
Sayli29 <sayli.tawhare@gmail.com>
2024-07-14 10:59:55 +0530

    feat: added basic form from shadcn with zod

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import PatientForm from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
+import Link  from "next/link";
 import Image from "next/image";
 
 export default function Home() {
@@ -13,8 +15,24 @@ export default function Home() {
             alt="patient"
             className="mb-12 h-10 w-ft"
           />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end  text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+      <Image
+        src='/assets/images/onboarding-img.png'
+        height={1000}
+        width={1000}
+        alt='patient'
+        className="side-img max-w-[50%]"
+       />
     </div>
   );
 }
